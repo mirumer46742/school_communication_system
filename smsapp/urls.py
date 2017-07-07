3d477feb1b7dfd30eb408a4dbbc4d27bb3bf8643
Mir Umer <mirumershafi@gmail.com>
2017-07-07 13:11:06 +0530

    Changed Url for HOME

--- smsapp/urls.py
+++ smsapp/urls.py
@@ -13,7 +13,7 @@ from smsapp.views import BulkUploadView
 urlpatterns=[
 	url(r'^$',RegisterView.as_view(),name='register'),
 	url(r'^login/$',LoginView.as_view(),name='login'),
-	url(r'^dashboard/$', login_required(DashboardView.as_view()), name='dashboard'),
+	url(r'^home/$', login_required(DashboardView.as_view()), name='dashboard'),
 	url(r'^send/$', SendView.as_view(), name='send'),
 	url(r'^bulk_upload/$', BulkUploadView.as_view(), name='bulk_upload'),
 	url(r'^logout/$', LogoutView.as_view(), name='logout'),
